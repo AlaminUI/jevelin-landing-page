de574065072832f2966883baa8b9fc142efba6d1
AlaminUI <alaminislam907@gmail.com>
2022-12-11 12:51:11 +0600

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,28 +14,7 @@ document.querySelector('.navbar-toggler').addEventListener('click', function(){
 })
 
  
-
-// function hasScrolled() {
  
-//     // Make sure they scroll more than delta
-//     if(Math.abs(lastScrollTop - scrolltop) <= delta)	return;
-
-//     // If they scrolled down and are past the navbar, add class .nav-up.
-//     // This is necessary so you never see what is "behind" the navbar.
-//     if (scrolltop > lastScrollTop && scrolltop > navbarHeight){
-//       // On scroll down we add nav-up
-     
-//       $('header').removeClass('header-sticky').addClass('nav-up');
-    
-//     } else {
-//       // On scroll up we add header-sticky
-//       if(scrolltop + $(window).height() < $(document).height()) {
-//         $('header').removeClass('nav-up').addClass('header-sticky');
-//       }
-//       if(scrolltop <200){
-//         $('header').removeClass('header-sticky')
-//       }
-//     }
 
     document.addEventListener('scroll', function(){
         var scrolltop = $(this).scrollTop();
@@ -47,3 +26,15 @@ document.querySelector('.navbar-toggler').addEventListener('click', function(){
 
         }
     });
+
+    $(document).ready(function(){
+        $(window).load(function(){
+          $('.pre-loader').remove();
+        });
+        $(this).load(function(){
+            $('.pre-loader').remove();
+        });
+        $(document).load(function(){
+            $('.pre-loader').remove();
+        });
+      })
